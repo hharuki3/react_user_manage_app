0f6bedb3c08da2d3bc126a77a87680b5293e4140
hharuki3 <pingyanchunshu@gmail.com>
2024-07-15 22:58:38 +0900

    Homeページのルーティング設定

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,11 +1,26 @@
 import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { HomeRoutes } from "./HomeRoutes";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route
+        path="/home"
+        element={
+          <Routes>
+            {HomeRoutes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.children}
+              />
+            ))}
+          </Routes>
+        }
+      />
     </Routes>
   );
 });
